2384cc6c7038286c910d6c8dfbc09eb91e179473
Aditya <192105adityashah@gmail.com>
2024-03-23 19:17:51 +0530

    new pages

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
-import { HeroScrollDemo } from './components/Land/Land'; 
-import { TypewriterEffectSmoothDemo } from './components/Land/HeroText';
+import ECommerce from "@/components/Dashboard/E-commerce";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
+export const metadata: Metadata = {
+  title:
+    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
 
-export default function Home(){
+export default function Home() {
   return (
-    <main className='bg-white'>
-      <TypewriterEffectSmoothDemo />
-      <HeroScrollDemo />
-    </main>
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
   );
 }
