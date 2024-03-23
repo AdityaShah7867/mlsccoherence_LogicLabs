6058595d1743b5a0e76c82c1a36222e2cb988e41
Aditya Singh <singhadi280303@gmail.com>
2024-03-23 13:01:22 +0530

    content

--- Frontend/src/app/components/Navbar/Navbar.tsx
+++ Frontend/src/app/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="flex items-center justify-between border-b-2 border-gray-500 flex-wrap  bg-white py-4 lg:px-12 shadow border-solid border-t-2 ">
       <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid  border-gray-300 pb-5 lg:pb-0">
         <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
-          <span className="font-semibold text-xl tracking-tight">Harmony Homes</span>
+          <span className="font-semibold text-xl tracking-tight">InsightfulSocial</span>
         </div>
         <div className="block lg:hidden">
           <button
